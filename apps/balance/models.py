65ed9fc2d4affb730ae2736ea44339076f44a6e8
Aviah <aviahl@gmail.com>
2016-01-29 06:03:30 -0800

    balance models migs

--- apps/balance/models.py
+++ apps/balance/models.py
@@ -7,12 +7,12 @@ from site_repo.django_add.validators import verify_month_int
 
 class MonthlyBalance(models.Model):
     
-    month_to_balance = models.IntegerField(validators=[verify_month_int])
-    year_to_balance = models.IntegerField()
+    month_of_balance = models.IntegerField(validators=[verify_month_int])
+    year_of_balance = models.IntegerField()
     
     account = models.ForeignKey(Account,related_name="months_balanced")
     
-    divorcee1 = models.ForeignKey(User,blank=True,related_name="divorcee1_balance")
-    divorcee2 = models.ForeignKey(User,blank=True,related_name="divorcee2_balance")
+    divorcee1 = models.ForeignKey(User,blank=True,null=True,related_name="divorcee1_balance")
+    divorcee2 = models.ForeignKey(User,blank=True,null=True,related_name="divorcee2_balance")
     
     is_balanced = models.BooleanField()
