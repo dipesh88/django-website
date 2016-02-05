d6b601e63d6b9e773d7982d26a15d00fb8b8b81e
Aviah <aviahl@gmail.com>
2016-02-05 10:48:02 -0800

    fix wait socket server start

--- apps/tasks_queue/start_server.py
+++ apps/tasks_queue/start_server.py
@@ -1,10 +1,11 @@
 from .app_settings import TASKS_HOST,TASKS_PORT
 from . import worker_manager
 from .server import TaskSocketServerThread
-
+import time
 
 worker_manager.start()
 server_thread = TaskSocketServerThread(TASKS_HOST,TASKS_PORT)
+time.sleep(5)
 socket_server = server_thread.socket_server()
 socket_server.serve_forever()
 
