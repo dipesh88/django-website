b68089c846d41faac8df1f1d64a88015e96a9775
Aviah <aviahl@gmail.com>
2016-02-27 22:33:34 -0800

    demo account read only

--- django_add/context_processors.py
+++ django_add/context_processors.py
@@ -1,7 +1,12 @@
 # custom context processors
+from django.conf import settings
 
 def custom_context(request):
     """
     Custom items that are available to the templates
     """
-    return {'DJANGO_VER': 'django 1.8.7 LTS'}
+    
+    return {
+        'DJANGO_VER': 'django 1.8.7 LTS',
+        'DEMO': request.user.is_authenticated() and request.user.id in settings.DEMO_USERS
+            }
